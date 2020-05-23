83fbc2c28856d4a8c5dec1b826a2a7d1b880de96
Luke Wahlmeier <lwahlmeier@gmail.com>
2020-05-23 04:33:25 -0600

    fixed issue with RTP/RTCP over udp with nat between, reduced allocs when reading udp from source rtsp server

--- stream-udpl.go
+++ stream-udpl.go
@@ -8,6 +8,9 @@ import (
 
 type streamUdpListenerState int
 
+var _RTPPing = []byte{0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+var _RTCPPing = []byte{0x80, 0xc9, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}
+
 const (
 	_UDPL_STATE_STARTING streamUdpListenerState = iota
 	_UDPL_STATE_RUNNING
@@ -58,12 +61,25 @@ func (l *streamUdpListener) start() {
 	go l.run()
 }
 
+func (l *streamUdpListener) sendNATPing() {
+	dstAddr := &net.UDPAddr{
+		Port: l.publisherPort,
+		IP:   l.publisherIp,
+	}
+	if l.flow == _TRACK_FLOW_RTP {
+		l.nconn.WriteTo(_RTPPing, dstAddr)
+	} else if l.flow == _TRACK_FLOW_RTCP {
+		l.nconn.WriteTo(_RTCPPing, dstAddr)
+	}
+}
+
 func (l *streamUdpListener) run() {
+	l.sendNATPing()
+	lastNatTime := time.Now()
+	buf := make([]byte, 1024*64)
+
 	for {
-		// create a buffer for each read.
-		// this is necessary since the buffer is propagated with channels
-		// so it must be unique.
-		buf := make([]byte, 2048) // UDP MTU is 1400
+
 		n, addr, err := l.nconn.ReadFromUDP(buf)
 		if err != nil {
 			break
@@ -79,12 +95,24 @@ func (l *streamUdpListener) run() {
 
 			l.p.tcpl.forwardTrack(l.path, l.trackId, l.flow, buf[:n])
 		}()
+		// a new buffer slice is needed for each read.
+		// this is necessary since the buffer is propagated with channels
+		// so it must be unique.
+		buf = buf[n:]
+		if len(buf) < 2048 {
+			buf = make([]byte, 1024*64)
+		}
 
 		func() {
 			l.mutex.Lock()
 			defer l.mutex.Unlock()
 			l.lastFrameTime = time.Now()
 		}()
+		if time.Since(lastNatTime) >= time.Duration(30*time.Second) {
+			l.sendNATPing()
+			lastNatTime = time.Now()
+		}
+
 	}
 
 	close(l.done)
